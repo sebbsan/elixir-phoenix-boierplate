b6b05dcd0ef67b4dfbc6b7fa313cca1fc209a2c2
Sebastian <sebastian.weikart@gmail.com>
2017-06-28 09:00:04 +0200

    add comments show route

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,12 +4,20 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
+
+
 
   def index(conn, _params) do
-    IO.inspect(conn.assigns)
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
   end
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render conn, "show.html", topic: topic
+  end
+
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -60,4 +68,18 @@ defmodule Discuss.TopicController do
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
   end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+    if Repo.get!(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You can not edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
+  end
+
+
 end
